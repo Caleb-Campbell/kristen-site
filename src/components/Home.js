54ef14e9c21c18ac9276d118edc855a217d646d4
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-11-10 10:41:54 -0700

    imported framer motion. added spacer

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,13 @@ import React from 'react'
 import NavBar from './NavBar'
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper";
-import 'swiper/css'
+import { motion } from 'framer-motion';
 
+
+import 'swiper/css'
 import logo from '../assets/logo.jpg'
 import logoSmall from '../assets/logo-small.jpg'
+import kristen from '../assets/kristen.png'
 
 
 
@@ -22,7 +25,29 @@ function Home(props) {
         <img src= {logoSmall} className='title-img' />
         <button className='title-btn'>Katie is Hot</button>
       </div>
-      <section className='spacer'></section>
+      <section className='spacer'>
+        <motion.div
+        initial={{
+          opacity: 0,
+          x: -100
+        }}
+        animate={{
+          opacity: 1,
+          x: 0
+        }}
+        transition={{
+          duration: 2
+        }}
+
+        className='meet-kristen'>
+          <div className='meet-img-wrapper'>
+          <img className='meet-kristen-img' src={kristen} />
+          </div>
+          <div className='meet-kristen-text'>
+          <h5 className='meet-kristen-h'>Kristen Hatch at your service and the service of her fellow beings</h5>
+          </div>
+        </motion.div>
+      </section>
 
       <h4>This slider is broken dont worry. It will be something great someday</h4>
     <Swiper
