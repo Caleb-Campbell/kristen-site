bad5caafba4f6bfc953e9306798fd2e6ec5e20b6
Caleb Campbell <calebcampbellcrm@gmail.com>
2022-11-22 12:51:25 -0700

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,9 @@ import { motion } from 'framer-motion';
 import 'swiper/css'
 import logo from '../assets/logo.jpg'
 import logoSmall from '../assets/logo-small.jpg'
-import kristen from '../assets/kristen.png'
+
+//components
+import GallerySection from './GallerySection';
 
 
 
@@ -23,58 +25,35 @@ function Home(props) {
       <NavBar />
       <div className='main-title'>
         <img src= {logoSmall} className='title-img' />
-        <button className='title-btn'>Katie is Hot</button>
       </div>
-      <section className='spacer'>
-        <motion.div
-        initial={{
-          opacity: 0,
-          x: -100
-        }}
-        animate={{
-          opacity: 1,
-          x: 0
-        }}
-        transition={{
-          duration: 2
-        }}
+      {/* <GallerySection /> */}
+      <section className='spacer'></section>
+      <section className='gallery'>
 
-        className='meet-kristen'>
-          <div className='meet-img-wrapper'>
-          <img className='meet-kristen-img' src={kristen} />
-          </div>
-          <div className='meet-kristen-text'>
-          <h5 className='meet-kristen-h'>Kristen Hatch at your service and the service of her fellow beings</h5>
-          </div>
-        </motion.div>
       </section>
+      <section className='spacer'></section>
+      <section className='about-me'>
+      <h2 className='about-h'>About Me</h2>
+      <div className='about-wrap'>
+        <div className='about-img-wrap'>
+          <img className='about-img' />
+        </div>
+        <div className='about-p-wrap'>
+          <p className='about-p'>I'm Kristen Hatch. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        </div>
+      </div>
+      </section>
+      <section className='spacer'></section>
+      <section className='portfolio'>
 
-      <h4>This slider is broken dont worry. It will be something great someday</h4>
-    <Swiper
-        spaceBetween={1}
-        centeredSlides={true}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: false,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      <SwiperSlide className='slide'><img className='slide-img' src='https://picsum.photos/200' /></SwiperSlide>
-      ...
-    </Swiper>
+      </section>
+      <section className='spacer'></section>
+      <section className='client-testimonials'>
+
+      </section>
+      
+
+    
 
 
 
